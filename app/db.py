8f2eb778d8865e0f3ef3dadd17cd26a67d5c3197
Gabriel Carvalho <gabriel.silva@alu.mbitschool.com>
2025-12-14 20:34:30 +0100

    feat: database connection

---
+++ app/db.py
@@ -0,0 +1,24 @@
+import os
+from typing import Optional
+
+from sqlalchemy import Engine, create_engine
+
+_engine: Optional[Engine] = None
+
+
+def get_engine() -> Engine:
+
+   global _engine
+   if _engine is not None:
+      return _engine
+
+   host = os.getenv("MYSQL_HOST")
+   port = int(os.getenv("MYSQL_PORT"))
+   db = os.getenv("MYSQL_DATABASE")
+   user = os.getenv("MYSQL_USER")
+   password = os.getenv("MYSQL_PASSWORD")
+
+   _engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}")
+   return _engine
+
+
